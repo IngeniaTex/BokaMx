bc93b17fcf56fc01b58126439a558c4d0afed1af
Omar Yepez <ypz.omar@gmail.com>
2026-01-09 17:19:53 -0600

    code cleanup

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -10,13 +10,7 @@ import type { FC } from "react";
 import { useEffect, useState } from "react";
 import BackToTop from "../elements/BackToTop";
 import Footer1 from "./footer/Footer1";
-import Footer2 from "./footer/Footer2";
-import Footer3 from "./footer/Footer3";
-import Footer4 from "./footer/Footer4";
 import Header1 from "./header/Header1";
-import Header2 from "./header/Header2";
-import Header3 from "./header/Header3";
-import Header4 from "./header/Header4";
 import { useRevealAnimation } from "../../util/useRevealAnimation";
 // Define the props interface (same as above)
 interface BootstrapComponentsProps {}
@@ -72,16 +66,10 @@ export default function Layout({ headerStyle, footerStyle, mainMenuStyle, childr
       <BootstrapComponents />
       {!headerStyle && <Header1 mainMenuStyle={mainMenuStyle} scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} offcanvas_bg={offcanvas_bg} offcanvas_menu={offcanvas_menu} offcanvas_social={offcanvas_social} />}
       {headerStyle == 1 ? <Header1 mainMenuStyle={mainMenuStyle} scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} offcanvas_bg={offcanvas_bg} offcanvas_menu={offcanvas_menu} offcanvas_social={offcanvas_social} /> : null}
-      {headerStyle == 2 ? <Header2 mainMenuStyle={mainMenuStyle} scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} offcanvas_bg={offcanvas_bg} offcanvas_menu={offcanvas_menu} offcanvas_social={offcanvas_social} /> : null}
-      {headerStyle == 3 ? <Header3 mainMenuStyle={mainMenuStyle} scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} offcanvas_bg={offcanvas_bg} offcanvas_menu={offcanvas_menu} offcanvas_social={offcanvas_social} /> : null}
-      {headerStyle == 4 ? <Header4 mainMenuStyle={mainMenuStyle} scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} offcanvas_bg={offcanvas_bg} offcanvas_menu={offcanvas_menu} offcanvas_social={offcanvas_social} /> : null}
-
+    
       <main>{children}</main>
       {!footerStyle && <Footer1 />}
       {footerStyle == 1 ? <Footer1 /> : null}
-      {footerStyle == 2 ? <Footer2 /> : null}
-      {footerStyle == 3 ? <Footer3 /> : null}
-      {footerStyle == 4 ? <Footer4 /> : null}
 
       <BackToTop />
     </>
